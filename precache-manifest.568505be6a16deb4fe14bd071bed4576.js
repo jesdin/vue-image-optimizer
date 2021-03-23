de8d28c49f7308a6c8d217f8d61091aa73b9907b
jesdin <jesdinraphael@gmail.com>
2021-03-23 14:36:43 +0000

    Deploying to gh-pages from @ jesdin/vue-image-optimizer@c99bb71f5af8c3ccb5439778bc8f99fdc8799159 🚀

---
+++ precache-manifest.568505be6a16deb4fe14bd071bed4576.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ef91543c653d7e7190ce1c14f8cb8b",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "abbaa881e5f5952c1baa",
+    "url": "/css/app.409c771c.css"
+  },
+  {
+    "revision": "3800ea939759c32fd2f7",
+    "url": "/css/chunk-vendors.f4cb1d50.css"
+  },
+  {
+    "revision": "fb6625d4ac6839917cefa3addc6b7f2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abbaa881e5f5952c1baa",
+    "url": "/js/app.c03fac51.js"
+  },
+  {
+    "revision": "3800ea939759c32fd2f7",
+    "url": "/js/chunk-vendors.7e6d699a.js"
+  },
+  {
+    "revision": "7da6ae0f1e02404619bba015bacdb7ce",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- src/events/OptimizationRequestedEvent.ts
+++
@@ -1,9 +0,0 @@
-import { UppyFile } from "@uppy/core";
-
-export interface OptimizationRequestedEvent {
-    width: number
-    height: number
-    quality: number
-    shouldMaintainAspectRatio: boolean
-    files: Array<UppyFile<{}, {}>>
-}
